9343ca088f4900faa7c2bb0e68d130714ec1eae5
Izaan Sohail <izaansohail1999@gmail.com>
2022-07-19 12:32:51 +0500

    sorting css

--- hospital_rehabilitation_service/src/App.js
+++ hospital_rehabilitation_service/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/Login" element={<Login/>} />
+          <Route exact path="/" element={<Login/>} />
           
         </Routes>
       </BrowserRouter>
--- hospital_rehabilitation_service/src/Login.js
+++ hospital_rehabilitation_service/src/Login.js
@@ -5,53 +5,52 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Login = () => {
-    const [email,setEmail] = useState('');
-    const [pass,setPass] = useState('');
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
     const axios = require('axios');
-    const isValid = async()=>{
-        let result = await fetch('http://localhost:8080/Login',{
-            method:'post',
+    const isValid = async () => {
+        let result = await fetch('http://localhost:8080/Login', {
+            method: 'post',
             body: JSON.stringify({
-                "email":email,
-                "password":pass
+                "email": email,
+                "password": pass
             }),
-            headers:{
-                'Content-Type':'application/json'
+            headers: {
+                'Content-Type': 'application/json'
             }
         });
         result = await result.json();
-        if(result.post){
+        if (result.post) {
             console.log('successful');
-        }else{
+        } else {
             console.log('not successful');
         }
         console.log(result);
-        
+
     }
     return (
         <div className="login-bg">
-            <div id="wrap">
+            <div id="wrap" style={{ marginTop: "15vw" }}>
                 <div className="loginParentContainer">
                     <div className="subContainer">
-                        <div style={{textAlign:"center"}}>
+                        <div style={{ textAlign: "center" }}>
                             <img src={Logo} alt="Stonestep Logo" />
+                            <h4>Rehabilitation Service Provider Directory</h4>
                         </div>
-                        <h2>Rehabilitation Service Provider Directory</h2>
                         <div className="loginForm">
                             <label for="">Email ID</label>
-                            <input onChange={(e)=>setEmail(e.target.value)} type="text" className="inputborder-btm field" /><br /><br />
+                            <input onChange={(e) => setEmail(e.target.value)} type="text" className="inputborder-btm field" /><br /><br />
                             <label for="">Password</label>
-                            <input onChange={(e)=>setPass(e.target.value)} type="password" className="field" /><br />
-                            <Link className="forgot" to="#_">Forgot Password?</Link>
-
-                            <button onClick={isValid} type="submit" className="Loginbtn " >Login</button>
+                            <input onChange={(e) => setPass(e.target.value)} type="password" className="field" /><br />
+                            <div style={{ textAlign: "center" }}>
+                                <Link className="forgot" to="#_">Forgot Password?</Link>
+                            </div>
+                            <button onClick={isValid} type="submit" className="Loginbtn" >Login</button>
 
                         </div>
 
                     </div>
                 </div>
-
-
             </div>
         </div>
     );
