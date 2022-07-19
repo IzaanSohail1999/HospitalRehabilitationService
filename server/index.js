1bc44a1ef03be8cecbbe37aae3fe14e66af3b5b1
Izaan Sohail <izaansohail1999@gmail.com>
2022-07-19 17:51:02 +0500

    forget password email api made

--- server/index.js
+++ server/index.js
@@ -47,5 +47,63 @@ app.post('/Login',async(req,res)=>{
     }
  })
 
+ app.post('/ForgetPassword',async(req,res)=>{
+    if(req.body.content && req.body.subject){
+        sendEmail(req.body.subject, req.body.content)
+        res.status(406).send({result:'Success'});
+    }else{
+         res.status(406).send({result:'Invalid Information'});
+    }
+ })
+
+    const nodemailer = require('nodemailer');
+      let transporter = nodemailer.createTransport({
+             host: 'smtp.mailtrap.io',
+             port: 2525,
+             auth: {
+                user: "24bf5736da2cf2",
+                pass: "0670d8d721aed5"
+             }
+     })
+
+     function sendEmail(subject, content) {
+        message = {
+            from: "from-example@email.com",
+            to: "to-example@email.com",
+            subject: subject,
+            text: content
+       }
+       transporter.sendMail(message, function(err, info) {
+            if (err) {
+              console.log(err)
+            } else {
+              console.log(info);
+            }
+     })
+    }
+
+//  function setupRoutes() {
+//     const routes = require("./routes");
+//     routes.setup(app);
+  
+//     app.use(function(err, req, res, next) {
+//       res.status(err.headerStatus || err.status || 403);
+//       res.send({
+//         status: err.status || 403,
+//         message: err.message || err,
+//         data: err.data || {}
+//       });
+//     });
+  
+//     // catch 404 and forward to error handler
+//     app.use(function (req, res, next) {
+//       res.status(200);
+//       // res.sendFile(path.join(__dirname + '/index.html'));
+//       res.sendFile('index.html', { root: path.join(__dirname, '../public') });
+//   });
+// }
+
+// sendEmail()
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
